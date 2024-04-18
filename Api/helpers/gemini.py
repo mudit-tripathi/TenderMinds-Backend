9ee547ec94cb4abb95eaa7213ea691ed60a1fc53
Bhuvaneswari Kavoori <bhumikakavoori@gmail.com>
2024-04-18 12:45:47 +0530

    updated chat_gemini to block none

--- Api/helpers/gemini.py
+++ Api/helpers/gemini.py
@@ -8,7 +8,10 @@ from Api.constant.constants import GOOGLE_API_KEY
 async def chat_gemini(prompt):
     try : 
         llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0, google_api_key=GOOGLE_API_KEY,transport='rest',safety_settings={
-        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE
     })
         result = llm.invoke(prompt)
         return result.content
